7e47933c76362ff311024070f5ed5a3d9ec74427
Matthew Leder <matthew.leder@ledermail.com>
2018-03-16 21:07:54 +0100

    add new projects page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (request, response) => {
   });
 });
 
+app.get('/projects', (request, response) => {
+  response.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+})
+
 function contact(request, response) {
   response.send('Contact page');
 }
